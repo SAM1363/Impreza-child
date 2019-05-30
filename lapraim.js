b38f141c83115293ac02b610c6d01260e0f74510
Samson <isamu3636136@gmail.com>
2019-05-30 13:10:06 -0500

    finish responsive

--- lapraim.js
+++ lapraim.js
@@ -1,28 +1,40 @@
-jQuery(document).scroll(function () {
-		scrollHeight = jQuery(document).height(); 
-		scrollPosition = jQuery(window).height() + jQuery(window).scrollTop(); 
-		footHeight = jQuery("#footer").innerHeight(); 
-		headHeight = jQuery("header").innerHeight(); 
+// jQuery(document).scroll(function () {
+// 		scrollHeight = jQuery(document).height(); 
+// 		scrollPosition = jQuery(window).height() + jQuery(window).scrollTop(); 
+// 		footHeight = jQuery("#footer").innerHeight(); 
+// 		headHeight = jQuery("header").innerHeight(); 
 
-		// if( scrollHeight - scrollPosition  > footHeight ) { 
-		// 	jQuery(".social").css({
-		// 		"position": "fixed",
-		// 		"top": "200px",
-		// 		"right": "-50px",
-		// 		'transition': '.5s'
-		// 		});
-		// }else {
-		// 	// jQuery(".social").addClass("move");
-		// 	jQuery(".social").css({
-		// 		"position": "fixed",
-		// 		// "position": "abslute",
-		// 		"top": "150px",
-		// 		"right": "-50px",
-		// 		'transition': '.5s'
-		// 		});
-		// }
+// 		if( scrollHeight - scrollPosition  > footHeight ) { 
+// 			jQuery(".social").css({
+// 				"position": "fixed",
+// 				"top": "200px",
+// 				"right": "-50px",
+// 				'transition': '.5s'
+// 				});
+// 		}else {
+// 			jQuery(".social").addClass("move");
+// 			jQuery(".social").css({
+// 				"position": "fixed",
+// 				"top": "150px",
+// 				"right": "-50px",
+// 				'transition': '.5s'
+// 				});
+// 		}
 		
+// });
+
+
+function checkOffset() {
+	if(jQuery('.social').offset().top + jQuery('.social').height() >= jQuery('footer').offset().top - 10)
+			jQuery('.social').css('position', 'absolute');
+	if(jQuery(document).scrollTop() + window.innerHeight < jQuery('footer').offset().top -20)
+			jQuery('.social').css('position', 'fixed'); 
+}
+
+jQuery(document).scroll(function() {
+	checkOffset();
 });
+
 
 let scroll_pos = 0;
 jQuery(window).scroll(function () {
@@ -39,8 +51,10 @@ jQuery(window).scroll(function () {
 		winScrollTop = jQuery(this).scrollTop();
     if (winScrollTop >= startPos) {
         jQuery('.header-wraper').addClass('hide');
+        jQuery('.social').addClass('hide');
     } else {
         jQuery('.header-wraper').removeClass('hide');
+        jQuery('.social').removeClass('hide');
     }
     startPos = winScrollTop;
 });
